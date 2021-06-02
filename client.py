b3b9ae219732db0f242f348370eadff1a7c8f74d
Josie Jitzel Alvarez <josiealv@usc.edu>
2021-06-01 18:50:14 -0700

    changes in authorization

--- client.py
+++ client.py
@@ -1,5 +1,9 @@
+import json
 import os
+import requests
 
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-AUTH_REQUEST = "https://accounts.spotify.com/authorize?client_id=" + CLIENT_ID +"&response_type=code&redirect_uri="
+
+
+
