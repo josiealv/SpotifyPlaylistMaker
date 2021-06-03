a88a4fea4b68a6adf534f8a29ea0eaa584aae957
Josie Jitzel Alvarez <josiealv@usc.edu>
2021-06-02 19:22:27 -0700

    added the parameters needed to generate authorization token and changed client to be a class

--- client.py
+++ client.py
@@ -5,5 +5,9 @@ import requests
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
+class Client:
+    def __init__(self, auth_token, user_id):
+        self.user_id = user_id
+        self.auth_token = auth_token
 
 
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+from client import Client
 
 def main():
     user_id = input("Please enter user id/username (NOTE: this is different from your display name, this can be found in account ovierview)\n")
     print ("Go to this site: https://developer.spotify.com/console/post-playlists/ \n")
     print ("Enter user_id in empty 'user_id' text box & click 'Get Token' button \n")
-    print ("")
+    print ("Select the following:\n")
+    print ("playlist-modify-public\n")
+    print ("playlist-modify-private\n")
+#  print ("Select the following:\n")
     auth_token = input ("Enter genereated OAuth Token:\n")
+    cli = Client(auth_token, user_id)
