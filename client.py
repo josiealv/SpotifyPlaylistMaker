33730fccb5b219ebce371a72f1b21974223abc5e
Josie Jitzel Alvarez <josiealv@usc.edu>
2021-06-06 14:21:22 -0700

    finished

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import json
 import os
 import requests
 from track import Track
+from playlist import Playlist
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
@@ -10,6 +11,9 @@ id_str = "id"
 artists = "artists"
 items = "items"
 track_str = "track"
+tracks_str = "tracks"
+url = "external_urls"
+spotify = "spotify"
 class Client:
     def __init__(self, auth_token, user_id):
         self.user_id = user_id
@@ -30,7 +34,7 @@ class Client:
             data=data,
             headers={
                 "Content-Type": "application/json",
-                "Authorization": f"Bearer {self._authorization_token}"
+                "Authorization": f"Bearer {self.auth_token}"
             }
         )
         return response
@@ -41,3 +45,31 @@ class Client:
         tracks = [Track(track[track_str][name], track[track_str][artists][0][name], track[track_str][id_str]) for
                   track in json_response[items]] # tracks might be items instead
         return tracks
+
+    def get_recommedations(self, seeds, limit=15):
+        seeds_url = ""
+        for seed in seeds:
+            seeds_url += seed.id + ","
+        endpoint = f"https://api.spotify.com/v1/recommendations?seed_tracks={seeds_url}&limit={limit}"
+        response = self.get_api_request(endpoint)
+        json_response = response.json()
+        recommendations = [Track(track[name], track[artists][0][name], track[id_str]) for
+                  track in json_response[tracks_str]]
+        return recommendations
+
+    def create_playlist (self, name, playlist_tracks):
+        data_playlist = json.dumps({
+            "name": name,
+            "description": "bangerz",
+            "public": True
+        })
+        endpoint_playlist = f"https://api.spotify.com/v1/users/{self.user_id}/playlists"
+        response = self.post_api_request(endpoint_playlist, data_playlist)
+        json_response = response.json()
+        playlist = Playlist(name, json_response[id_str], json_response[url][spotify])
+
+        track_uris = [track.create_spotify_uri() for track in playlist_tracks]
+        data_add_tracks = json.dumps(track_uris)
+        endpoint_add_tracks = f"https://api.spotify.com/v1/playlists/{playlist.id}/tracks"
+        response = self.post_api_request(endpoint_add_tracks, data_add_tracks)
+        return playlist
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from client import Client
 
 def main():
     user_id = input("Please enter user id/username (NOTE: this is different from your display name, this can be found in account ovierview)\n")
+    print("\n")
     print ("Go to this site: https://developer.spotify.com/console/post-playlists/ ")
     print ("Enter user_id in empty 'user_id' text box & click 'Get Token' button ")
     print ("Select the following:")
@@ -10,12 +11,32 @@ def main():
     print ("user-read-recently-played")
     auth_token = input ("Enter genereated OAuth Token: \n")
     cli = Client(auth_token, user_id)
+    print("\n")
 
-    num_tracks = int(input("How many of your recently played tracks would you like to see? (Min: 10, Max: 50) \n"))
+    num_tracks = int(input("How many of your recently played tracks would you like to see? (Min: 10, Max: 50) "))
     last_tracks = cli.get_last_played_tracks(num_tracks)
+    print("\n")
 
-    print("Here are your last "+ str(num_tracks) + " played:")
+    print(f"Here are your last {num_tracks} played:")
     for i, track in enumerate(last_tracks):
         print(f"{i+1}. {track.name} by {track.artist}")
+    print("\n")
 
+    indexes = input(f"Enter the number of the tracks, separated by a comma, that you want to base the playlist around (Min: 1, Max: {num_tracks}) ")
+    indexes = indexes.split(",")
+    seeds = [last_tracks[int (i)-1] for i in indexes]
+    num_recs = int (input ("How many recommended tracks would you like? (Min: 1, Max: 100) "))
+    recommnded_tracks = cli.get_recommedations(seeds, num_recs)
+    print("\n")
+
+    print(f"Here are your {num_recs} recommneded tracks")
+    for i, track in enumerate(recommnded_tracks):
+        print(f"{i+1}. {track.name} by {track.artist}")
+    print("\n")
+
+    recommnded_tracks += seeds
+    playlist_name = input("Enter the name of your playlist: ")
+    playlist = cli.create_playlist(playlist_name, recommnded_tracks)
+    print(f"Created {playlist.name} with recommendded tracks successfully!")
+    print(f"url: {playlist.url}")
 main()
