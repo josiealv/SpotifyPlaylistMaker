3c5557d7a2b6338f6a132a8c3b9dbf86c34fbefe
Josie Jitzel Alvarez <josiealv@usc.edu>
2021-06-06 19:19:29 -0700

    refined the recommended tracks

--- client.py
+++ client.py
@@ -3,8 +3,6 @@ import os
 import requests
 from track import Track
 from playlist import Playlist
-CLIENT_ID = os.getenv("CLIENT_ID")
-CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
 name = "name"
 id_str = "id"
@@ -14,6 +12,11 @@ track_str = "track"
 tracks_str = "tracks"
 url = "external_urls"
 spotify = "spotify"
+audio_features = "audio_features"
+mode_str = "mode"
+energy = "energy"
+tempo = "tempo"
+valence = "valence"
 class Client:
     def __init__(self, auth_token, user_id):
         self.user_id = user_id
@@ -38,6 +41,32 @@ class Client:
             }
         )
         return response
+    
+    def get_recommendations_audio_features (self, json_response, total_seeds, seeds_url, limit):
+        instrumentalness = 0
+        mode = 0
+        energy_avg = 0
+        tempo_avg = 0
+        valence_avg = 0
+        mode1 = 0
+        mode0 = 0
+        for audio in json_response[audio_features]:
+            if(audio[mode_str]==1):
+                mode1+=1
+            else:
+                mode0+=1
+            energy_avg += audio[energy]
+            tempo_avg += audio[tempo]
+            valence_avg += audio[valence]
+        mode = max(mode1, mode0)
+        energy_avg /= total_seeds
+        tempo_avg /= total_seeds
+        valence_avg /= total_seeds
+        
+        endpoint_seeds = f"https://api.spotify.com/v1/recommendations?seed_tracks={seeds_url}&target_mode={mode}&target_energy={energy_avg}&target_tempo={tempo_avg}&target_valence={valence_avg}&limit={limit}"
+        response = self.get_api_request(endpoint_seeds)
+        return response.json()
+
     def get_last_played_tracks(self, limit=15):
         endpoint = f"https://api.spotify.com/v1/me/player/recently-played?limit={limit}" 
         response = self.get_api_request(endpoint)
@@ -47,12 +76,14 @@ class Client:
         return tracks
 
     def get_recommedations(self, seeds, limit=15):
+        #instrumentalness, mode, energy, tempo, valence, 
         seeds_url = ""
         for seed in seeds:
             seeds_url += seed.id + ","
-        endpoint = f"https://api.spotify.com/v1/recommendations?seed_tracks={seeds_url}&limit={limit}"
-        response = self.get_api_request(endpoint)
-        json_response = response.json()
+        endpoint_audio_features = f"https://api.spotify.com/v1/audio-features?seed_tracks={seeds_url}"
+        response = self.get_api_request(endpoint_audio_features)
+        json_response = self.get_recommendations_audio_features(response.json(), len(seeds), seeds_url, limit)
+        print(json_response)
         recommendations = [Track(track[name], track[artists][0][name], track[id_str]) for
                   track in json_response[tracks_str]]
         return recommendations
